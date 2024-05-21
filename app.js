828b3ac63eb02db0776f8ec0f1932c2341687b19
Ramesha-sheikh <165018589+Ramesha-sheikh@users.noreply.github.com>
2024-05-21 23:55:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+console.log(chalk.yellowBright("\n\t\t\t\t*******************************************************************************************************"));
+console.log(chalk.greenBright("\n\t\t\t\t\t\t!!!!!!!!!!!!!!!number gussing game!!!!!!!!!!!!!!!!!"));
+console.log(chalk.yellowBright("\n\t\t\t\t*******************************************************************************************************"));
+import chalk from "chalk";
+import inquirer from "inquirer";
+let system_generate = Math.floor(Math.random() * 5 + 1);
+const userinput = await inquirer.prompt([
+    {
+        type: "number",
+        name: "user_guess",
+        message: "\n\t\t\t\t\t\tenter your gussing number 1 to 6 = ",
+        prefix: " ",
+    },
+]);
+const { user_guess } = userinput;
+console.log(chalk.blueBright(`\n\t\t\t\t\t ${user_guess}    you are gussing number , system generate is    ${system_generate}`));
+if (userinput.user_guess === system_generate) {
+    console.log(chalk.greenBright("\n\t\t\t\t\tyou are win game"));
+}
+else {
+    console.log(chalk.redBright("\n\t\t\t\t\t\t\t\tTRY AGIAN"));
+}
+console.log(chalk.cyanBright("\n\t\t\t\t\t______________________ XXXX_________XXXX________XXXX________XXXX_________________________"));
